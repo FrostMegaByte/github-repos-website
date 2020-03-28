558ac97f45da01121146cfaa426df6059dcad03c
Mark Bekooy <mark.bekooy@gmail.com>
2020-03-28 20:40:53 +0100

    Split development and production code with 2 npm commands

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,19 +1,12 @@
+const common = require("./webpack.common");
+const merge = require("webpack-merge");
 const path = require("path");
 
-module.exports = {
+module.exports = merge(common, {
   mode: "development",
   devtool: "none", // Remove eval() from dist/main.js
-  entry: "./src/app.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
-  },
-};
+});
