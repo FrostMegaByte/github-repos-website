9d5d9bff020ae159b8601403c5f4307fb14c53a4
Mark Bekooy <mark.bekooy@gmail.com>
2020-03-28 22:53:26 +0100

    Rename template.html and create table of github repositories

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
 import "./main.scss";
 
+const repositoriesTable = document.getElementById("repositories");
+
 fetch("https://api.github.com/users/FrostMegaByte/repos")
   .then((response) => {
     return response.json();
   })
   .then((data) => {
+    let repositoriesData = `<tr>
+      <th>Name</th>
+      <th>Description</th>
+      <th>Created</th>
+    </tr>`;
+
     for (const repo of data) {
-      const repoInformation = [repo.name, repo.description, repo.created_at];
-      console.log(repoInformation.toString());
+      const repoInformation = `<tr>
+        <td>${repo.name}</td>
+        <td>${repo.description}</td>
+        <td>${repo.created_at}</td>
+      </tr>`;
+      repositoriesData += repoInformation;
     }
+    repositoriesTable.innerHTML = repositoriesData;
   });
